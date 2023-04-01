2d696f4c2ead55df25182fddc74f67e420267dd6
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-04-01 10:23:41 +0600

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,24 @@
+ #include <iostream>
+ #include <conio.h>
+
+ using namespace std;
+
+ int main()
+
+ {
+     float num1 =10.5, num2 = 20.5;
+     char ch;
+
+     ch =  'a';
+
+     cout<< "Number 1 = " <<num1 <<endl;
+
+     cout<< "Number 2 = " <<num2 <<endl;
+
+     cout<< "Charecter = " << ch;
+
+      getch();
+
+
+
+ }
